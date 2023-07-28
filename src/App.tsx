c7b9cb44ba2478584adea9e31ba8a9bc679c462a
sourav.sharma <sourav.sharma@netsmartz.net>
2023-07-28 15:46:17 +0530

    banner added

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,29 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import SingleProduct from "./components/SingleProduct/SingleProduct";
 import AppContext from "./utils/context";
+import Banner from "./components/Home/Banner/Banner";
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <AppContext value={undefined}>
         <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/category/:id" element={<Category />} />
-          <Route path="/products/:id" element={<SingleProduct />} />
-        </Routes>
+        <Banner />
+        <Router />
         <Newsletter />
         <Footer />
       </AppContext>
     </BrowserRouter>
+  );
+}
+
+function Router(): JSX.Element {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/category/:id" element={<Category />} />
+      <Route path="/products/:id" element={<SingleProduct />} />
+    </Routes>
   );
 }
 
